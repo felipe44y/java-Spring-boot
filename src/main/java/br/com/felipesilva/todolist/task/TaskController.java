21d425c1205024570d42c1252dcf32070317d73f
felipe44y <felipecesar777.dev@gmail.com>
2025-09-23 16:07:51 -0300

    validetion the date in tasks

--- src/main/java/br/com/felipesilva/todolist/task/TaskController.java
+++ src/main/java/br/com/felipesilva/todolist/task/TaskController.java
@@ -4,12 +4,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.servlet.http.HttpServletRequest;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 @RequestMapping("/tasks")
@@ -19,13 +23,29 @@ public class TaskController {
     private ITaskRepository taskRepository;
 
     @PostMapping("/create")
-    public TaskModel Create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
+    public ResponseEntity<Object> Create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
         System.out.println("Chegou no controller" );
         var idUser = request.getAttribute("userId");
         taskModel.setIdUser((UUID)idUser);
-        var task = this.taskRepository.save(taskModel);
-        return task;
 
+        var currentDate = LocalDateTime.now();
+        if(currentDate.isAfter(taskModel.getStartAt()) || currentDate.isAfter(taskModel.getEndAt())) {
+          return ResponseEntity.status(400).body("A data de início deve ser futura.");
+        }   
+
+        if(taskModel.getStartAt().isAfter(taskModel.getEndAt())) {
+          return ResponseEntity.status(400).body("A data de início deve ser anterior à data de término.");
+        } 
+        
+        var task = this.taskRepository.save(taskModel); 
+        return ResponseEntity.status(201).body(task);
+    }
+
+    @GetMapping("/list")
+    public List<TaskModel> list(HttpServletRequest request){
+        var idUser = request.getAttribute("userId");
+        var taskList = this.taskRepository.findByIdUser((UUID) idUser);
+        return taskList;
     }
 }
-  
+   
